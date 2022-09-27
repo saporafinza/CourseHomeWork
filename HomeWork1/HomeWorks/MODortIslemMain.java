310ab71b0da1767f9cf5d212c108f8016935e37e
saporafinza <mahmutsami14562@gmail.com>
2022-09-28 00:04:02 +0300

    MethodOverloading.java

---
+++ HomeWork1/HomeWorks/MODortIslemMain.java
@@ -0,0 +1,12 @@
+package HomeWorks;
+
+public class MODortIslemMain {
+
+	public static void main(String[] args) {
+		
+		MODortIslem moDortIslem = new MODortIslem();
+		System.out.println(moDortIslem.Topla(2, 3, 4));
+		
+	}
+
+}
