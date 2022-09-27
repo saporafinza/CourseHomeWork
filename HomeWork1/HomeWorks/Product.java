36dcad24ac63f99884e866beddcf16d98d4987c1
saporafinza <mahmutsami14562@gmail.com>
2022-09-27 23:32:24 +0300

    Product.java

---
+++ HomeWork1/HomeWorks/Product.java
@@ -0,0 +1,11 @@
+package HomeWorks;
+
+public class Product {
+	
+	int id;
+	String name;
+	String description;
+	double price;
+	int stockAmount;
+
+}
---
+++ HomeWork1/HomeWorks/ProductManager.java
@@ -0,0 +1,11 @@
+package HomeWorks;
+
+public class ProductManager {
+	
+	public void Add(Product product) {
+		
+		System.out.println("Ürün eklendi " + product.name);
+		
+	}
+	
+}
